b02c9fc9ff4cfc2bcd6cd1c12f119e7674781d5f
Guido Eckelt <guido.eckelt@gmail.com>
2016-10-30 18:44:01 +0100

    #2 guidoeckelt/Rollerei@14fbf6906d6f672e5c79960f1cd8a9c8308e65f5 change correction

--- src/FrontEndBundle/Controller/AdminController.php
+++ src/FrontEndBundle/Controller/AdminController.php
@@ -26,7 +26,7 @@ class AdminController extends Controller
      */
     public function startseiteAction()
     {
-        return $this->redirectToRoute('admin.video');
+        return $this->redirectToRoute('admin.videos');
     }
 
 
